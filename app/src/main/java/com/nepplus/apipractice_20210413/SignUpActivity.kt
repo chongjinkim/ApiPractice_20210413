e9957060ffa48241aec51e3b526abc81f181ee52
김종진 <martia0118@gmail.com>
2021-04-13 23:56:57 +0900

    회원가입 화면 추가

---
+++ app/src/main/java/com/nepplus/apipractice_20210413/SignUpActivity.kt
@@ -0,0 +1,24 @@
+package com.nepplus.apipractice_20210413
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SignUpActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+        setupEvetns()
+        setValue()
+    }
+
+    override fun setupEvetns() {
+
+    }
+
+    override fun setValue() {
+
+    }
+
+
+}
